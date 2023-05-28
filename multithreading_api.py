c75cb87b0a5bf86afda3371491739f1bb018440f
watchakorn-18k <porton2559@gmail.com>
2023-05-28 22:38:20 +0700

    Update any node api

--- multithreading_api.py
+++ multithreading_api.py
@@ -5,9 +5,9 @@ import time
 import threading
 import argparse
 import os
-from dotenv import load_dotenv
+import dotenv
 
-load_dotenv()
+dotenv.load_dotenv()
 
 
 with open(
@@ -67,9 +67,27 @@ def translat_with_gpt(prompt: str, api_point: int):
             time.sleep(5)
 
 
+def status_api():
+    print("STATUS API : {}".format(len(api_endpoints_all)))
+    for i in api_endpoints_all:
+        if requests.get(i).json().get("Hello") == "GPT-PROJECT":
+            print("🟢", i, "- READY !")
+
+
+def recheck_api_again():
+    while True:
+        all_responses_valid = True
+        for endpoint in api_endpoints_all:
+            response = requests.get(endpoint).json()
+            if response.get("Hello") != "GPT-PROJECT":
+                all_responses_valid = False
+                break
+        if all_responses_valid:
+            break
+
+
 def loop_file_multithread(path_filename: str):
     global api_endpoints_all
-
     file_path = path_filename
     filename = os.path.basename(file_path)
     with open(path_filename, "r", encoding="utf-8") as f:
@@ -82,21 +100,11 @@ def loop_file_multithread(path_filename: str):
             lst.append(i)
         with open(f"SRT_FILE/{filename}", "a", encoding="utf-8") as f:
             f.writelines(lst)
-    print("STATUS API : {} NODES".format(len(api_endpoints_all)))
-    for i in api_endpoints_all:
-        if requests.get(i).json().get("Hello") == "GPT-PROJECT":
-            print("🟢", i, "- READY !")
-    print("STARTING.....")
-    while True:
-        all_responses_valid = True
-        for endpoint in api_endpoints_all:
-            response = requests.get(endpoint).json()
-            if response.get("Hello") != "GPT-PROJECT":
-                all_responses_valid = False
-                break
 
-        if all_responses_valid:
-            break
+    status_api()
+    # recheck_api_again()
+
+    print("STARTING.....")
     # loop data in file
     with open(path_filename, "r", encoding="utf-8") as file:
         # Count the number of lines in the file
@@ -106,49 +114,45 @@ def loop_file_multithread(path_filename: str):
     file = open(path_filename, "r", encoding="utf-8")
     lines = file.readlines()
     num_iterations = len(api_endpoints_all)  # total number of iterations
+
+    def t_1():
+        for line in lines[:split_in_half]:
+            translate_and_append(filename, line, progress_bar, 1)
+
+    def t_2():
+        for line in lines[split_in_half : split_in_half * 2]:
+            translate_and_append(filename, line, progress_bar, 2)
+
+    def translate_lines(i):
+        return lambda: [translate_and_append(filename, line, progress_bar, i) for line in lines[split_in_half * (i-1) : split_in_half * i]]
+    
+    def translate_lines_end(i):
+        return lambda: [translate_and_append(filename, line, progress_bar, {i}) for line in lines[split_in_half * {i} :]]
     for i in range(num_iterations):
+        i += 1
+        if i > 2:
+            globals()[f"t_{i}"] = translate_lines(i)
+        elif i == num_iterations:
+            globals()[f"t_{i}"] = translate_lines_end(i)
 
-        def t_i(i=i):
-            if i == 1:
-                for line in lines[:split_in_half]:
-                    translate_and_append(filename, line, progress_bar, 1)
-            elif i == num_iterations:
-                for line in lines[split_in_half * (num_iterations - 1) :]:
-                    translate_and_append(
-                        filename, line, [], progress_bar, num_iterations
-                    )
-            else:
-                start = (i - 1) * split_in_half
-                end = i * split_in_half
-                for line in lines[start:end]:
-                    translate_and_append(filename, line, progress_bar, i)
-
-        globals()[f"t_{i+1}"] = t_i
     # Calculate split_in_half dynamically
     split_in_half = len(lines) // num_iterations
     if len(lines) % num_iterations != 0:
         split_in_half += 1
 
-    threads = []
-    for i in range(1, num_iterations + 1):
-        threads.append(threading.Thread(target=globals()[f"t_{i}"]))
-
-    # start
-    for thread in threads:
-        thread.start()
-
-    # Wait for both threads to finish
-    for thread in threads:
-        thread.join()
+    for i in range(num_iterations):
+        i += 1
+        exec(f"task_{i} = threading.Thread(target=t_{i})")
+    for i in range(num_iterations):
+        i += 1
+        exec(f"task_{i}.start()")
+    for i in range(num_iterations):
+        i += 1
+        exec(f"task_{i}.join()")
 
     progress_bar.close()
-    file.close()
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--filename", type=str, required=True)
-    args = parser.parse_args()
-    path_filename = args.filename
-
+    path_filename = "G:\FILE_ANIME\ORC-FOR_SUBTTITLE\File_srt\lolsf-14.ass"
     loop_file_multithread(path_filename)
